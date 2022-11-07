b274d80d55169bc331517e10975f7a5e5c04f278
Jack <raditywd@gmail.com>
2022-11-07 22:39:40 +0800

    pertama

---
+++ js/music-list.js
@@ -0,0 +1,488 @@
+let allMusic = [
+    {
+        name : "Miss You",
+        artis : "Oliver Tree & Robin Schuiz",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name : "Mary On A Cross",
+        artis : "Ghost",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name : "Somewhere Only We Know",
+        artis : "Keane",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name : "Another Love",
+        artis : "Tom Odell",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name : "Heat Waves",
+        artis : "Glass Animals",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name : "Dandelions",
+        artis : "Ruth B",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name : "Bad Habit",
+        artis : "Steve Lacy",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name : "After Hours",
+        artis : "The Weeknd",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name : "Wiped Out!",
+        artis : "The Neighbourhood",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name : "When I Was Your Man",
+        artis : "Bruno Mars",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name : "Sempurna",
+        artis : "Andra & The Backbone",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name : "L",
+        artis : "Hal",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name : "R U MINE?",
+        artis : "Artic Monkeys",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name : "DJ Hamil Duluan",
+        artis : "Srch : Yt",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name : "DJ No Comment Aku",
+        artis : "Srch : Yt",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name : "DJ Goyang Dumang",
+        artis : "Srch : Yt",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name : "DJ Goyang Dumang #2",
+        artis : "Srch : Yt",
+        img: "music-17",
+        src: "music-17"
+    },
+    {
+        name : "DJ Berondong Tua",
+        artis : "Srch : Yt",
+        img: "music-18",
+        src: "music-18"
+    },
+    {
+        name : "Homage",
+        artis : "Mild High Club",
+        img: "music-19",
+        src: "music-19"
+    },
+    {
+        name : "Sweater weather",
+        artis : "The Neighbourhood",
+        img: "music-20",
+        src: "music-20"
+    },
+    {
+        name : "Understand",
+        artis : "BoyWithUke",
+        img: "music-21",
+        src: "music-21"
+    },
+    {
+        name : "Loafers",
+        artis : "BoyWithUke",
+        img: "music-22",
+        src: "music-22"
+    },
+    {
+        name : "Kakai kitan",
+        artis : "Eve",
+        img: "music-23",
+        src: "music-23"
+    },
+    {
+        name : "Best Part ",
+        artis : "Daniel Caesar",
+        img: "music-24",
+        src: "music-24"
+    },
+    {
+        name : "it's yours",
+        artis : "NCT",
+        img: "music-25",
+        src: "music-25"
+    },
+    {
+        name : "What I Think Of",
+        artis : "Chris Andrian Yang",
+        img: "music-26",
+        src: "music-26"
+    },
+    {
+        name : "just a little bit",
+        artis : "enhypen",
+        img: "music-27",
+        src: "music-27"
+    },
+    {
+        name : "At My Worst",
+        artis : "Pink Sweat$",
+        img: "music-28",
+        src: "music-28"
+    },
+    {
+        name : "Memories (One Piece)",
+        artis : "Red Rocket",
+        img: "music-29",
+        src: "music-29"
+    },
+    {
+        name : "Spaceman",
+        artis : "MewSuppasit",
+        img: "music-30",
+        src: "music-30"
+    },
+    {
+        name : "DNA",
+        artis : "BTS",
+        img: "music-31",
+        src: "music-31"
+    },
+    {
+        name : "Heart Shaker",
+        artis : "Twice",
+        img: "music-32",
+        src: "music-32"
+    },
+    {
+        name : "What Sort of Ending Are You Wishing For?",
+        artis : "TUYU",
+        img: "music-33",
+        src: "music-33"
+    },
+    {
+        name : "Suki dakara",
+        artis : "Yuika",
+        img: "music-34",
+        src: "music-34"
+    },
+    {
+        name : "TONDEMO-WONDERZ",
+        artis : "KAITO",
+        img: "music-35",
+        src: "music-35"
+    },
+    {
+        name : "Living On The Edge",
+        artis : "ALKALOID/天城",
+        img: "music-36",
+        src: "music-36"
+    },
+    {
+        name : "Hated by life",
+        artis : "カンザキイオリ",
+        img: "music-37",
+        src: "music-37"
+    },
+    {
+        name : "Monëy so big",
+        artis : "Yeat",
+        img: "music-38",
+        src: "music-38"
+    },
+    {
+        name : "Dancin",
+        artis : "Aaron Smith",
+        img: "music-39",
+        src: "music-39"
+    },
+    {
+        name : "Murder In My Mind",
+        artis : "Kordhell",
+        img: "music-40",
+        src: "music-40"
+    },
+    {
+        name : "Midnight Phonk",
+        artis : "Xanakin Skywok",
+        img: "music-41",
+        src: "music-41"
+    },
+    {
+        name : "Space Song",
+        artis : "Beach House",
+        img: "music-42",
+        src: "music-42"
+    },
+    {
+        name : "After Dark",
+        artis : "Mr.Kitty",
+        img: "music-43",
+        src: "music-43"
+    },
+    {
+        name : "I Wanna Be Yours",
+        artis : "Artic Monkeys",
+        img: "music-44",
+        src: "music-44"
+    },
+    {
+        name : "505",
+        artis : "Artic Monkeys",
+        img: "music-45",
+        src: "music-45"
+    },
+    {
+        name : "This Side of Paradise",
+        artis : "Coyote Theory",
+        img: "music-46",
+        src: "music-46"
+    },
+    {
+        name : "Tek It",
+        artis : "Cafuné",
+        img: "music-47",
+        src: "music-47"
+    },
+    {
+        name : "We fell in love in october",
+        artis : "girl in red",
+        img: "music-48",
+        src: "music-48"
+    },
+    {
+        name : "Until I Found You",
+        artis : "Stephen Sanchez",
+        img: "music-49",
+        src: "music-49"
+    },
+    {
+        name : "I Love You So",
+        artis : "The Walters",
+        img: "music-50",
+        src: "music-50"
+    },
+    {
+        name : "Losing Interest",
+        artis : "Shiloh Dynasty",
+        img: "music-51",
+        src: "music-51"
+    },
+    {
+        name : "DeathBed",
+        artis : "Powfu",
+        img: "music-52",
+        src: "music-52"
+    },
+    {
+        name : "double take",
+        artis : "Dhruv",
+        img: "music-53",
+        src: "music-53"
+    },
+    {
+        name : "Animals",
+        artis : "Maroon 5",
+        img: "music-54",
+        src: "music-54"
+    },
+    {
+        name : "Play Date",
+        artis : "Melanie Martine",
+        img: "music-55",
+        src: "music-55"
+    },
+    {
+        name : "Dusk Till Dawm",
+        artis : "Zayn Malik",
+        img: "music-56",
+        src: "music-56"
+    },
+    {
+        name : "Take Away",
+        artis : "The Chainsmokers",
+        img: "music-57",
+        src: "music-57"
+    },
+    {
+        name : "Unity",
+        artis : "Alan Walker",
+        img: "music-58",
+        src: "music-58"
+    },
+    {
+        name : "Lemon Tree",
+        artis : "Gustixa",
+        img: "music-59",
+        src: "music-59"
+    },
+    {
+        name : "Fallen Kingdom",
+        artis : "Gustixa",
+        img: "music-60",
+        src: "music-60"
+    },
+    {
+        name : "Goodbye To A World",
+        artis : "Gustixa",
+        img: "music-61",
+        src: "music-61"
+    },
+    {
+        name : "Past Lives",
+        artis : "Sapientdream",
+        img: "music-62",
+        src: "music-62"
+    },
+    {
+        name : "Atlantis",
+        artis : "SeaFret",
+        img: "music-63",
+        src: "music-63"
+    },
+    {
+        name : "ERROR{JS music-list not found}",
+        artis : "ERROR{JS music-list not found}",
+        img: "music-64",
+        src: "music-64"
+    },
+    {
+        name : "Night Changes",
+        artis : "One Direction",
+        img: "music-65",
+        src: "music-65"
+    },
+    {
+        name : "Strong",
+        artis : "One Direction",
+        img: "music-66",
+        src: "music-66"
+    },
+    {
+        name : "Someone You Loved",
+        artis : "Lewis Capaldi",
+        img: "music-67",
+        src: "music-67"
+    },
+    {
+        name : "It Will Rain",
+        artis : "Bruno Mars",
+        img: "music-68",
+        src: "music-68"
+    },
+    {
+        name : "Easy on Me",
+        artis : "Adele",
+        img: "music-69",
+        src: "music-69"
+    },
+    {
+        name : "Rockabye",
+        artis : "Clean Bandit",
+        img: "music-70",
+        src: "music-70"
+    },
+    {
+        name : "Symphony",
+        artis : "Clean Bandit",
+        img: "music-71",
+        src: "music-71"
+    },
+    {
+        name : "Wolves",
+        artis : "Marshmello dan Selena Gomez",
+        img: "music-72",
+        src: "music-72"
+    },
+    {
+        name : "Prom Queen",
+        artis : "Beach Bunny",
+        img: "music-73",
+        src: "music-73"
+    },
+    {
+        name : "Asmalibrasi",
+        artis : "Soegi Bornean",
+        img: "music-74",
+        src: "music-74"
+    },
+    {
+        name : "Evaluasi",
+        artis : "Hindia",
+        img: "music-75",
+        src: "music-75"
+    },
+    {
+        name : "Secukupnya",
+        artis : "Hindia",
+        img: "music-76",
+        src: "music-76"
+    },
+    {
+        name : "Monokrom",
+        artis : "Tulus",
+        img: "music-77",
+        src: "music-77"
+    },
+    {
+        name : "Hati-Hati di Jalan",
+        artis : "TULUS",
+        img: "music-78",
+        src: "music-78"
+    },
+    {
+        name : "Thank You [Sloweb + Rain]",
+        artis : "Stan Walker",
+        img: "music-79",
+        src: "music-79"
+    },
+    {
+        name : "Build A Bi****",
+        artis : "Bella Poarch",
+        img: "music-80",
+        src: "music-80"
+    },
+    {
+        name : "Total Semua Musik 81",
+        artis : "Create by Rodot",
+        img: "music-81",
+        src: "music-81"
+    },
+]
